30ed7ea7081b20da8799c2dca338728dbc0c2467
joelcrawford <joelrootcrawford@gmail.com>
2020-06-16 16:37:40 -0700

    adding lambda function

--- express/serverless.js
+++ express/serverless.js
@@ -1,5 +1,6 @@
 "use strict";
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 
 app.use(express.static("public"));
@@ -8,4 +9,6 @@ app.get("/", function (req, res) {
   res.sendfile("public/index.html");
 });
 
-app.listen(80, () => console.log("Gator app listening on port 80!"));
+//app.listen(80, () => console.log("Gator app listening on port 80!"));
+
+module.exports.handler = serverless(app);

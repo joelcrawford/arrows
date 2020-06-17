452c20ccbe5f7a450f5479ffe70f645a1e7c8906
joelcrawford <joelrootcrawford@gmail.com>
2020-06-17 09:59:50 -0700

    reverse order of router and static

--- express/server.js
+++ express/server.js
@@ -9,7 +9,6 @@ const router = express.Router();
 router.get("/", (_, res) => {
   res.sendFile("public/index.html", { root: __dirname });
 });
-app.use("/", express.static(path.join(__dirname, "..", "public")));
 
 // app.get("/", function (_, res) {
 //   res.sendFile("public/index.html", { root: __dirname });
@@ -17,6 +16,7 @@ app.use("/", express.static(path.join(__dirname, "..", "public")));
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", express.static(path.join(__dirname, "..", "public")));
 app.use("/", (_, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;

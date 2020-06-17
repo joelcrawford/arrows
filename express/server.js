3f342b49fbd1d2464070ff4ce7621df672c01536
joelcrawford <joelrootcrawford@gmail.com>
2020-06-17 09:44:15 -0700

    testing express server without redirects

--- express/server.js
+++ express/server.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "..", "public")));
 
-app.get("/", function (_, res) {
-  res.sendFile("public/index.html", { root: __dirname });
-});
+// app.get("/", function (_, res) {
+//   res.sendFile("public/index.html", { root: __dirname });
+// });
 
 module.exports = app;
 module.exports.handler = serverless(app);

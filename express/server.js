3e8d526a92cf119538a3c353495a5dfc2ab264a5
joelcrawford <joelrootcrawford@gmail.com>
2020-06-17 09:53:26 -0700

    trying express router

--- express/server.js
+++ express/server.js
@@ -2,13 +2,22 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
+const router = express.Router();
+router.get("/", (_, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
 app.use("/", express.static(path.join(__dirname, "..", "public")));
 
 // app.get("/", function (_, res) {
 //   res.sendFile("public/index.html", { root: __dirname });
 // });
+
+app.use(bodyParser.json());
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (_, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
